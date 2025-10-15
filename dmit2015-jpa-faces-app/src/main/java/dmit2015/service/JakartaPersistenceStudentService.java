507dbdd0bd07e9d6ebca0866c4b552620d998a50
Sam Wu <swu@nait.ca>
2025-10-15 17:37:05 -0600

    Added Jakarta Persistence implmentation of model service interface.

---
+++ dmit2015-jpa-faces-app/src/main/java/dmit2015/service/JakartaPersistenceStudentService.java
@@ -0,0 +1,90 @@
+package dmit2015.service;
+
+import dmit2015.model.Student;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Named;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+
+import java.util.List;
+import java.util.Optional;
+
+import java.util.UUID;
+import java.util.random.RandomGenerator;
+
+@Named("jakartaPersistenceStudentService")
+@ApplicationScoped
+public class JakartaPersistenceStudentService implements StudentService {
+
+    // Assign a unitName if there are more than one persistence unit defined in persistence.xml
+    @PersistenceContext (unitName="postgresql-jpa-pu")
+    private EntityManager entityManager;
+
+    @Override
+    @Transactional
+    public Student createStudent(Student student) {
+        // If the primary key is not an identity column then write code below here to
+        // 1) Generate a new primary key value
+        // 2) Set the primary key value for the new entity
+
+        student.setId(UUID.randomUUID().toString());
+        entityManager.persist(student);
+        return student;
+    }
+
+    @Override
+    public Optional<Student> getStudentById(String id) {
+        try {
+            Student querySingleResult = entityManager.find(Student.class, id);
+            if (querySingleResult != null) {
+                return Optional.of(querySingleResult);
+            }
+        } catch (Exception ex) {
+            // id value not found
+            throw new RuntimeException(ex);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return entityManager.createQuery("SELECT o FROM Student o ", Student.class)
+                .getResultList();
+    }
+
+    @Override
+    @Transactional
+    public Student updateStudent(Student student) {
+
+        Optional<Student> optionalStudent = getStudentById(student.getId());
+        if (optionalStudent.isEmpty()) {
+            String errorMessage = String.format("The id %s does not exists in the system.", student.getId());
+            throw new RuntimeException(errorMessage);
+        } else {
+            var existingStudent = optionalStudent.orElseThrow();
+            // Update only properties that is editable by the end user
+
+            existingStudent.setFullName(student.getFullName());
+            existingStudent.setCourseTermSection(student.getCourseTermSection());
+
+            student = entityManager.merge(existingStudent);
+        }
+        return student;
+    }
+
+    @Override
+    @Transactional
+    public void deleteStudentById(String id) {
+        Optional<Student> optionalStudent = getStudentById(id);
+        if (optionalStudent.isPresent()) {
+            Student student = optionalStudent.orElseThrow();
+            // Write code to throw a RuntimeException if this entity contains child records
+            entityManager.remove(student);
+        } else {
+            throw new RuntimeException("Could not find Student with id: " + id);
+        }
+    }
+
+}
