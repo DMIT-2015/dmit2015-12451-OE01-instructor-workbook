f6ebc9590542c7e6cd7e76e70389698593b88c9b
Sam Wu <swu@nait.ca>
2025-10-22 17:41:53 -0600

    Added DepartmentConverter.java

---
+++ dmit2015-oracle-hr-app/src/main/java/dmit2015/faces/DepartmentConverter.java
@@ -0,0 +1,47 @@
+package dmit2015.faces;
+
+import dmit2015.entity.Department;
+import dmit2015.persistence.HumanResourcesRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.component.UIComponent;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.convert.Converter;
+import jakarta.faces.convert.ConverterException;
+import jakarta.faces.convert.FacesConverter;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+@Named
+@ApplicationScoped
+@FacesConverter(value="departmentConverter", managed=true)
+public class DepartmentConverter implements Converter<Department> {
+
+    @Inject
+    private HumanResourcesRepository hrRepository;
+
+    @Override
+    public Department getAsObject(FacesContext facesContext, UIComponent uiComponent, String value) {
+        if (value != null && !value.isBlank()) {
+           try {
+               Short departmentId = Short.valueOf(value);
+               return hrRepository.departmentByDepartmentId(departmentId);
+           } catch (NumberFormatException e) {
+               throw new ConverterException(
+                       new FacesMessage(
+                               FacesMessage.SEVERITY_ERROR,
+                               "Conversion Error",
+                               "Not a valid department id"));
+           }
+        }
+        return null;
+    }
+
+    @Override
+    public String getAsString(FacesContext facesContext, UIComponent uiComponent, Department department) {
+        if (department != null) {
+            return department.getId().toString();
+        }
+        return "";
+    }
+}
